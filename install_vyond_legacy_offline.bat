9fcc06d1921edf3353cae13c87ca26996ec24dd3
grapzmen-GoAnimation <arthurhenriqueesr@gmail.com>
2025-10-24 19:36:34 -0300

    Update install_vyond_legacy_offline.bat

--- install_vyond_legacy_offline.bat
+++ install_vyond_legacy_offline.bat
@@ -1,2 +1,3 @@
 @echo off
 echo This Requires Git, Get Git: https://github.com/git-for-windows/git/releases/download/v2.51.1.windows.1/Git-2.51.1-64-bit.exe
+git clone https://github.com/grapzmen-GoAnimation/Vyond-Legacy-Offline.git
